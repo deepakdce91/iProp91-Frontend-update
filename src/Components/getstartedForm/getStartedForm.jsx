b3e97e5b040bc523128808ffabcd82188ef7c76d
adityazyzz <tuskersadi@gmail.com>
2024-12-11 23:33:41 +0530

    journey form final

--- src/Components/getstartedForm/getStartedForm.jsx
+++ src/Components/getstartedForm/getStartedForm.jsx
@@ -4,7 +4,7 @@ import JourneyForm from '../Journey/JourneyForm';
 
 function JourneyPage() {
   return (
-    <div className='pt-[14vh] h-[90vh]'>
+    <div className='pt-[14vh] min-h-[90vh] bg-black'>
       <JourneyForm/>
     </div>
   )
