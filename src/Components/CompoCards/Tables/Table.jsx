ed847552822995a5959dcaaf37aa242e368116b5
ayush <ayushbhatt204@gmail.com>
2024-12-13 12:48:17 +0530

    documents upload bugs fixed

--- src/Components/CompoCards/Tables/Table.jsx
+++ src/Components/CompoCards/Tables/Table.jsx
@@ -4,10 +4,11 @@ import { jwtDecode } from 'jwt-decode';
 import { uploadFileToCloud, getSignedUrlForPrivateFile } from "../../../config/s3client";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Spinner } from '@material-tailwind/react';
 
 
 
-const Table = ({ tablename, category, tableopen = false}) => {
+const Table = ({ tablename, category, tableopen = false, loading }) => {
   const [isExpanded, setIsExpanded] = useState(tableopen);
   const [maxHeight, setMaxHeight] = useState(tableopen ? 'auto' : '0px');
   const [tableData, setdata] = useState([]);
@@ -25,7 +26,7 @@ const Table = ({ tablename, category, tableopen = false}) => {
 
   useEffect(() => {
     // Automatically adjust height when tableData changes
-    if (isExpanded) {
+    if (isExpanded && contentRef.current) {
       setMaxHeight(`${contentRef.current.scrollHeight}px`);
     }
   }, [tableData, isExpanded]);
@@ -35,6 +36,10 @@ const Table = ({ tablename, category, tableopen = false}) => {
   useEffect(() => {
     fetchSafeDate();
   }, [tableData]);
+
+  useEffect(() => {
+    fetchSafeDate(); // Fetch data whenever the category changes
+  }, [category]); // Add category as a dependency
 
   const fetchSafeDate = async () => {
 
@@ -203,91 +208,97 @@ const Table = ({ tablename, category, tableopen = false}) => {
 
   return (
     <>
-      <div className={`${!isExpanded ? 'border-2 rounded-xl border-gray-600 ' : ''} w-full   `}>
-        <div
-          className={`${!isExpanded ? '' : 'bg-white text-black border-[1px] border-black/20 '} p-4 rounded-t-lg flex text-black  justify-between items-center w-full cursor-pointer px-10 py-6`}
-          
-        >
-          <h2 className={`${isExpanded ? 'text-black font-semibold' : ''} text-sm md:text-2xl font-semibold text-center w-full`}>
-            {tablename}
-          </h2>
-          {/* <button className={`text-black transition-all delay-150 duration-50 font-bold text-2xl ${!isExpanded ? 'transform rotate-180 text-white' : 'text-white'}`}>
-            <FaChevronUp />
-          </button> */}
-          <button onClick={() => document.getElementById('dropzone-file').click()} className="ml-4 text-gray-700 text-2xl">
-            <i className="fas fa-plus"></i>
-          </button>
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <Spinner size={40} className="animate-spin" />
         </div>
+      ) : (
+        <div className={`${!isExpanded ? 'border-2 rounded-xl border-gray-600 ' : ''} w-full`}>
+          <div
+            className={`${!isExpanded ? '' : 'bg-white text-black border-[1px] border-black/20 '} p-4 rounded-t-lg flex text-black  justify-between items-center w-full cursor-pointer px-10 py-6`}
+            
+          >
+            <h2 className={`${isExpanded ? 'text-black font-semibold' : ''} text-sm md:text-2xl font-semibold text-center w-full`}>
+              {tablename}
+            </h2>
+            {/* <button className={`text-black transition-all delay-150 duration-50 font-bold text-2xl ${!isExpanded ? 'transform rotate-180 text-white' : 'text-white'}`}>
+              <FaChevronUp />
+            </button> */}
+            <button onClick={() => document.getElementById('dropzone-file').click()} className="ml-4 text-gray-700 text-2xl">
+              <i className="fas fa-plus"></i>
+            </button>
+          </div>
 
-        <div
-          ref={contentRef}
-          className="overflow-hidden transition-all duration-500 ease-in-out"
-          style={{ maxHeight: `${maxHeight}` }}
-        >
-          <div className="bg-white border-[1px] border-black/20 shadow-md rounded-b-lg w-full overflow-x-scroll no-scrollbar text-sm lg:text-lg">
-            <table className="w-full  text-left border-collapse">
-              <thead>
-                <tr className="bg-[#f3f3f3] text-black">
-                  <th className="py-3 px-6">No.</th>
-                  <th className="py-3 px-6">Name</th>
-               
-                  <th className="py-3 px-6">Date</th>
-                  <th className="py-3 px-6">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {tableData?.map((file, index) => (
-                  <tr key={index} className="">
-                    <td className="py-3 px-6">{index + 1}.</td>
-                    <td className="py-3 px-6" title={file.name}>
-                      {textWrap(file.name, 5)}
-                    </td>
-                    <td className="py-3 px-6" title={file.updatedAt}>{
-                      file.updatedAt ? new Date(file.updatedAt).toLocaleDateString() : new Date().toLocaleDateString()}
-                    </td>
-                    <td className="py-3 px-6 flex space-x-4" >
-                      <button onClick={() => handleView(file)}>
-                        <i className="fas fa-eye text-gray-700"></i>
-                      </button>
-                      <button onClick={() => handleDownload(file)}>
-                        <i className="fas fa-download text-gray-700"></i>
-                      </button>
-                      <button onClick={() => handleDelete(file)}>
-                        <i className="fas fa-trash text-gray-700"></i>
-                      </button>
-                    </td>
+          <div
+            ref={contentRef}
+            className="overflow-hidden transition-all duration-500 ease-in-out"
+            style={{ maxHeight: `${maxHeight}` }}
+          >
+            <div className="bg-white border-[1px] border-black/20 shadow-md rounded-b-lg w-full overflow-x-scroll no-scrollbar text-sm lg:text-lg">
+              <table className="w-full  text-left border-collapse">
+                <thead>
+                  <tr className="bg-[#f3f3f3] text-black">
+                    <th className="py-3 px-6">No.</th>
+                    <th className="py-3 px-6">Name</th>
+                   
+                    <th className="py-3 px-6">Date</th>
+                    <th className="py-3 px-6">Actions</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {tableData?.map((file, index) => (
+                    <tr key={index} className="">
+                      <td className="py-3 px-6">{index + 1}.</td>
+                      <td className="py-3 px-6" title={file.name}>
+                        {textWrap(file.name, 5)}
+                      </td>
+                      <td className="py-3 px-6" title={file.updatedAt}>{
+                        file.updatedAt ? new Date(file.updatedAt).toLocaleDateString() : new Date().toLocaleDateString()}
+                      </td>
+                      <td className="py-3 px-6 flex space-x-4" >
+                        <button onClick={() => handleView(file)}>
+                          <i className="fas fa-eye text-gray-700"></i>
+                        </button>
+                        <button onClick={() => handleDownload(file)}>
+                          <i className="fas fa-download text-gray-700"></i>
+                        </button>
+                        <button onClick={() => handleDelete(file)}>
+                          <i className="fas fa-trash text-gray-700"></i>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
 
-            <div className="relative flex items-center justify-center w-full">
-              <input
-                id="dropzone-file"
-                type="file"
-                className="absolute inset-0 opacity-0 cursor-pointer"
-                onChange={handleFileChange}
-                multiple
-              />
-              <label
-                htmlFor="dropzone-file"
-                className="flex flex-col items-center justify-center w-full h-40 cursor-pointer"
-              >
-                <div className="flex flex-col items-center justify-center">
-                  <button className="flex items-center text-black/40 border border-black/30 rounded-full p-2">
-                    <i className="fas fa-upload"></i>
-                  </button>
-                  <p className="text-black/40 mt-2">Click to Upload or drag and drop</p>
-                  {/* <p className="text-xs text-gray-500 dark:text-gray-400">(Max. File size: 25 MB)</p> */}
-                </div>
-              </label>
+              <div className="relative flex items-center justify-center w-full">
+                <input
+                  id="dropzone-file"
+                  type="file"
+                  className="absolute inset-0 opacity-0 cursor-pointer"
+                  onChange={handleFileChange}
+                  multiple
+                />
+                <label
+                  htmlFor="dropzone-file"
+                  className="flex flex-col items-center justify-center w-full h-40 cursor-pointer"
+                >
+                  <div className="flex flex-col items-center justify-center">
+                    <button className="flex items-center text-black/40 border border-black/30 rounded-full p-2">
+                      <i className="fas fa-upload"></i>
+                    </button>
+                    <p className="text-black/40 mt-2">Click to Upload or drag and drop</p>
+                    {/* <p className="text-xs text-gray-500 dark:text-gray-400">(Max. File size: 25 MB)</p> */}
+                  </div>
+                </label>
+              </div>
+
+
+
             </div>
-
-
-
           </div>
         </div>
-      </div>
+      )}
       <ToastContainer position="top-right" autoClose={2000} />
     </>
   );
